32a0e36cb1079d541790df1274545ab1564e2200
Kiran Murugulla <murugull@adobe.com>
2024-05-15 12:16:57 -0700

    intermediatory commit

--- .github/runbook/generate-runbook.mjs
+++ .github/runbook/generate-runbook.mjs
@@ -19,31 +19,38 @@ const main = async (token, targetDirectory) => {
 };
 const owner = 'kunwarsaluja';
 const repo = 'hackathon-runbook-stream';
-const checkSitemap = async (token) => {
+const sitemapUrl = `/repos/${owner}/${repo}/contents/helix-sitemap.yaml`;
+const checkSitemap = async (token, targetDirectory) => {
   const octokit = new Octokit({
     auth: token,
   });
-
+  const data = {
+    sitemapStatus: 'does not exists',
+  };
   try {
-    const res = await octokit.request(
-      'GET /repos/{owner}/{repo}/contents/helix-sitemap.yaml',
-      {
-        owner: owner,
-        repo: repo,
+    const res = await octokit.request(`GET ${sitemapUrl}`, {
+      owner: owner,
+      repo: repo,
+    });
+    if (res.status === 200) {
+      console.log(` Custom Sitemap available at ${sitemapUrl}`);
+      const resdata = JSON.parse(res.data);
+      console.log(resdata.url);
+      if (!fs.existsSync(targetDirectory)) {
+        fs.mkdirSync(targetDirectory);
       }
-    );
-    if (!res.ok) {
-      //throw new Error('Network response was not OK');
-      console.log(res.status);
-    } else {
-      const sitemapYaml = await JSON.stringify(res.data);
-      console.log(sitemapYaml);
+      data.sitemapStatus = 'exists';
+      data.url = resdata.url;
     }
+    fs.writeFileSync(
+      `${targetDirectory}/sitemapstatus.json`,
+      JSON.stringify(data)
+    );
   } catch (error) {
     console.error('error occured while fetching the sitemap config', error);
   }
 };
 
 const token = process.argv[2];
-checkSitemap(process.env.TOKEN).catch((e) => console.error(e));
+checkSitemap(process.env.TOKEN, '../../runbook').catch((e) => console.error(e));
 main(process.env.TOKEN, '../../runbook').catch((e) => console.error(e));
