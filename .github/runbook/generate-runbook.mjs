42640b38b04523bd07dcf9de5fbbbb7baf8c2aa3
Sean Steimer <ssteimer@adobe.com>
2024-05-15 12:11:02 -0700

    resturcture, get the first commit

--- .github/runbook/generate-runbook.mjs
+++ .github/runbook/generate-runbook.mjs
@@ -2,21 +2,34 @@
 import { Octokit } from "octokit";
 import fs from 'fs';
 
+const OCTOKIT_BASE_PARAMS = {
+  owner: "kunwarsaluja",
+  repo: "hackathon-runbook-stream",
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+};
+
+const getFirstCommit = async (octokit) => {
+  const commits = await octokit.paginate("GET /repos/{owner}/{repo}/commits", {
+    ...OCTOKIT_BASE_PARAMS,
+    path: '/scripts/scripts.js',
+  });
+  return commits.pop();
+};
+
 const main = async (token, targetDirectory) => {
   const octokit = new Octokit({
     auth: token,
   });
 
-  const res = await octokit.request("GET /repos/{owner}/{repo}/commits", {
-    owner: "kunwarsaluja",
-    repo: "hackathon-runbook-stream",
-  });
-  const data = JSON.stringify(res.data);
+  const data = {};
+  data.firstCommit = await getFirstCommit(octokit);
 
   if (!fs.existsSync(targetDirectory)) {
     fs.mkdirSync(targetDirectory);
   }
-  fs.writeFileSync(`${targetDirectory}/test.json`, data);
+  fs.writeFileSync(`${targetDirectory}/runbook-info.json`, JSON.stringify(data, undefined, 2));
 };
 
 const token = process.argv[2];
