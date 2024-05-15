6147d27a5277784fae970e3a1bdc14bc8e5aa1ed
Varun Mitra <vmitra@adobe.com>
2024-05-15 11:02:34 -0700

    Update generate-runbook.mjs

--- .github/runbook/generate-runbook.mjs
+++ .github/runbook/generate-runbook.mjs
@@ -11,6 +11,16 @@ const main = async (token, targetDirectory) => {
     repo: "hackathon-runbook-stream",
   });
   console.log(res.data);
+  const blocks = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+    owner: "kunwarsaluja",
+    repo: "hackathon-runbook-stream",
+    path: '/tree/main/blocks',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+  console.log(blocks.data);
+
 };
 
 const token = process.argv[2];
